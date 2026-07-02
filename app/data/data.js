a5f8e6b52af91c9fa44acff4462b2e668e50a042
Johan <cojalvallelenin919@gmail.com>
2026-07-01 23:53:41 -0500

    Agrego imagenes reales de Cajamarca y gastronomia

--- app/data/data.js
+++ app/data/data.js
@@ -24,43 +24,43 @@
     zona:"Celendin",
     img:"https://1.bp.blogspot.com/-KCWY2XsOhDs/W-GrQQP0rbI/AAAAAAAAC80/kS5aeqS3PKQj1KaSswAzHXicv16j_LhuQCLcBGAs/s1600/large_llanguat_termal.JPG"
   },
-
   {
     nombre:"Plaza de Armas de Cajamarca",
     zona:"Cajamarca",
-    img:"/images/cajamarca.svg"
+    img:"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0c/cc/2e/6e/vista-de-la-plaza-de.jpg?w=1200&h=1200&s=1"
   },
   {
     nombre:"Catedral de Cajamarca",
     zona:"Cajamarca",
-    img:"/images/cajamarca.svg"
+    img:"https://th.bing.com/th/id/OIP.m9xUep7gXqWcn3BxKkmz6gHaFe?o=7&rm=3&rs=1&pid=ImgDetMain"
   },
   {
     nombre:"Ventanillas de Otuzco",
     zona:"Cajamarca",
-    img:"/images/cajamarca.svg"
-  },
-  {
-    nombre:"Cuarto del Rescate",
-    zona:"Cajamarca",
-    img:"/images/cajamarca.svg"
-  },
-  {
-    nombre:"Banos del Inca",
-    zona:"Cajamarca",
-    img:"/images/cajamarca.svg"
+    img:"https://www.wamanadventures.com/blog/wp-content/uploads/2019/01/Las-Ventanillas-de-Otuzco-Waman-Adventures-3.jpg"
   }
 ];
 
 export const platos = [
-  { nombre:"Cuy frito", img:"/images/cajamarca.svg" },
-  { nombre:"Caldo verde", img:"/images/cajamarca.svg" },
-  { nombre:"Humitas", img:"/images/cajamarca.svg" },
-  { nombre:"Tamales", img:"/images/cajamarca.svg" }
+  {
+    nombre:"Cuy frito",
+    img:"https://www.peruenvideos.com/wp-content/uploads/2015/11/cuy-frito-receta-y-preparacion-cajamarca-peru.jpg"
+  },
+  {
+    nombre:"Caldo verde",
+    img:"https://tse4.mm.bing.net/th/id/OIP.qNFPvpfOPWTMEy6psPiM0wHaD4?rs=1&pid=ImgDetMain&o=7&rm=3"
+  },
+  {
+    nombre:"Humitas",
+    img:"https://tse2.mm.bing.net/th/id/OIP.FgtYeS4OyNg6oMtzpBzPFAHaEK?rs=1&pid=ImgDetMain&o=7&rm=3"
+  },
+  {
+    nombre:"Tamales",
+    img:"https://tse3.mm.bing.net/th/id/OIP.l8lnnDlv80JpuQEALUDghgHaD2?rs=1&pid=ImgDetMain&o=7&rm=3"
+  }
 ];
 
 export const fiestas = [
-  { nombre:"Carnaval de Cajamarca", img:"/images/cajamarca.svg" },
-  { nombre:"Carnaval de Celendin", img:"/images/celendin.svg" },
-  { nombre:"Comparsas tradicionales", img:"/images/cajamarca.svg" }
+  { nombre:"Carnaval de Cajamarca", img:"https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0c/cc/2e/6e/vista-de-la-plaza-de.jpg?w=1200&h=1200&s=1" },
+  { nombre:"Carnaval de Celendin", img:"https://i.pinimg.com/736x/8d/4f/0d/8d4f0db6325028e2f0d2828e8a54d479.jpg" }
 ];
