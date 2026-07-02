b7c6ac753ae788ce2ca949acddf53e36770fcc98
Johan <cojalvallelenin919@gmail.com>
2026-07-02 00:00:09 -0500

    Agrego menu hamburguesa

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,20 +1,30 @@
-﻿import Link from "next/link";
+﻿"use client";
+
+import { useState } from "react";
+import Link from "next/link";
 import styles from "./Navbar.module.css";
 
 export default function Navbar(){
+  const [open, setOpen] = useState(false);
+
   return(
     <header className={styles.navbar}>
       <Link href="/" className={styles.logo}>Celendin Cajamarca</Link>
-      <nav className={styles.menu}>
-        <Link href="/">Inicio</Link>
-        <Link href="/historia">Historia</Link>
-        <Link href="/turismo">Turismo</Link>
-        <Link href="/galeria">Galeria</Link>
-        <Link href="/mapa">Mapa</Link>
-        <Link href="/clima">Clima</Link>
-        <Link href="/gastronomia">Gastronomia</Link>
-        <Link href="/carnavales">Carnavales</Link>
-        <Link href="/contacto">Contacto</Link>
+
+      <button className={styles.hamburger} onClick={() => setOpen(!open)}>
+        ☰
+      </button>
+
+      <nav className={`${styles.menu} ${open ? styles.active : ""}`}>
+        <Link onClick={() => setOpen(false)} href="/">Inicio</Link>
+        <Link onClick={() => setOpen(false)} href="/historia">Historia</Link>
+        <Link onClick={() => setOpen(false)} href="/turismo">Turismo</Link>
+        <Link onClick={() => setOpen(false)} href="/galeria">Galeria</Link>
+        <Link onClick={() => setOpen(false)} href="/mapa">Mapa</Link>
+        <Link onClick={() => setOpen(false)} href="/clima">Clima</Link>
+        <Link onClick={() => setOpen(false)} href="/gastronomia">Gastronomia</Link>
+        <Link onClick={() => setOpen(false)} href="/carnavales">Carnavales</Link>
+        <Link onClick={() => setOpen(false)} href="/contacto">Contacto</Link>
       </nav>
     </header>
   );
