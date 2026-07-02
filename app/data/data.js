e17dd2749358e72eebb91f5a6070aa0f0bddc33f
Johan <cojalvallelenin919@gmail.com>
2026-07-01 22:57:41 -0500

    Corrijo imagenes turismo

--- app/data/data.js
+++ app/data/data.js
@@ -1,64 +1,15 @@
 ﻿export const destinos = [
-  {
-    nombre:"Plaza de Armas de Celendin",
-    zona:"Celendin",
-    img:"/images/celendin.jpg"
-  },
-  {
-    nombre:"Iglesia Matriz de Celendin",
-    zona:"Celendin",
-    img:"/images/celendin.jpg"
-  },
-  {
-    nombre:"Mirador Cristo Redentor",
-    zona:"Celendin",
-    img:"https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?auto=format&fit=crop&w=1200&q=80"
-  },
-  {
-    nombre:"Llanguat",
-    zona:"Celendin",
-    img:"https://images.unsplash.com/photo-1501785888041-af3ef285b470?auto=format&fit=crop&w=1200&q=80"
-  },
-  {
-    nombre:"Aguas Termales de Llanguat",
-    zona:"Celendin",
-    img:"https://images.unsplash.com/photo-1544551763-46a013bb70d5?auto=format&fit=crop&w=1200&q=80"
-  },
-  {
-    nombre:"Cumbemayo",
-    zona:"Cajamarca",
-    img:"https://commons.wikimedia.org/wiki/Special:FilePath/Cumbemayo.jpg"
-  },
-  {
-    nombre:"Banos del Inca",
-    zona:"Cajamarca",
-    img:"https://commons.wikimedia.org/wiki/Special:FilePath/Ba%C3%B1os%20del%20Inca.jpg"
-  },
-  {
-    nombre:"Ventanillas de Otuzco",
-    zona:"Cajamarca",
-    img:"https://commons.wikimedia.org/wiki/Special:FilePath/Ventanillas%20de%20Otuzco.jpg"
-  },
-  {
-    nombre:"Cuarto del Rescate",
-    zona:"Cajamarca",
-    img:"https://commons.wikimedia.org/wiki/Special:FilePath/Cuarto%20del%20Rescate.jpg"
-  },
-  {
-    nombre:"Granja Porcon",
-    zona:"Cajamarca",
-    img:"https://commons.wikimedia.org/wiki/Special:FilePath/Granja%20Porcon.jpg"
-  },
-  {
-    nombre:"Laguna San Nicolas",
-    zona:"Cajamarca",
-    img:"https://commons.wikimedia.org/wiki/Special:FilePath/Laguna%20San%20Nicolas%20Cajamarca.jpg"
-  },
-  {
-    nombre:"Kuntur Wasi",
-    zona:"Cajamarca",
-    img:"https://commons.wikimedia.org/wiki/Special:FilePath/Kuntur%20Wasi.jpg"
-  }
+  { nombre:"Plaza de Armas de Celendin", zona:"Celendin", img:"/images/celendin.jpg" },
+  { nombre:"Iglesia Matriz de Celendin", zona:"Celendin", img:"/images/celendin.jpg" },
+  { nombre:"Mirador Cristo Redentor", zona:"Celendin", img:"/images/celendin.jpg" },
+  { nombre:"Llanguat", zona:"Celendin", img:"/images/celendin.jpg" },
+  { nombre:"Aguas Termales de Llanguat", zona:"Celendin", img:"/images/celendin.jpg" },
+
+  { nombre:"Plaza de Armas de Cajamarca", zona:"Cajamarca", img:"/images/cajamarca.jpg" },
+  { nombre:"Catedral de Cajamarca", zona:"Cajamarca", img:"/images/cajamarca.jpg" },
+  { nombre:"Ventanillas de Otuzco", zona:"Cajamarca", img:"/images/cajamarca.jpg" },
+  { nombre:"Cuarto del Rescate", zona:"Cajamarca", img:"/images/cajamarca.jpg" },
+  { nombre:"Banos del Inca", zona:"Cajamarca", img:"/images/cajamarca.jpg" }
 ];
 
 export const platos = [];
