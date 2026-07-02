baf619c1308032afadde41fc6e6c170ff7e995db
Johan <cojalvallelenin919@gmail.com>
2026-07-01 23:01:49 -0500

    Corrijo imagenes con urls directas

--- app/data/data.js
+++ app/data/data.js
@@ -1,15 +1,15 @@
 ﻿export const destinos = [
-  { nombre:"Plaza de Armas de Celendin", zona:"Celendin", img:"/images/celendin.jpg" },
-  { nombre:"Iglesia Matriz de Celendin", zona:"Celendin", img:"/images/celendin.jpg" },
-  { nombre:"Mirador Cristo Redentor", zona:"Celendin", img:"/images/celendin.jpg" },
-  { nombre:"Llanguat", zona:"Celendin", img:"/images/celendin.jpg" },
-  { nombre:"Aguas Termales de Llanguat", zona:"Celendin", img:"/images/celendin.jpg" },
+  { nombre:"Plaza de Armas de Celendin", zona:"Celendin", img:"https://upload.wikimedia.org/wikipedia/commons/4/4d/Plaza_de_Armas_de_Celend%C3%ADn.jpg" },
+  { nombre:"Iglesia Matriz de Celendin", zona:"Celendin", img:"https://upload.wikimedia.org/wikipedia/commons/4/4d/Plaza_de_Armas_de_Celend%C3%ADn.jpg" },
+  { nombre:"Mirador Cristo Redentor", zona:"Celendin", img:"https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?auto=format&fit=crop&w=1200&q=80" },
+  { nombre:"Llanguat", zona:"Celendin", img:"https://images.unsplash.com/photo-1501785888041-af3ef285b470?auto=format&fit=crop&w=1200&q=80" },
+  { nombre:"Aguas Termales de Llanguat", zona:"Celendin", img:"https://images.unsplash.com/photo-1544551763-46a013bb70d5?auto=format&fit=crop&w=1200&q=80" },
 
-  { nombre:"Plaza de Armas de Cajamarca", zona:"Cajamarca", img:"/images/cajamarca.jpg" },
-  { nombre:"Catedral de Cajamarca", zona:"Cajamarca", img:"/images/cajamarca.jpg" },
-  { nombre:"Ventanillas de Otuzco", zona:"Cajamarca", img:"/images/cajamarca.jpg" },
-  { nombre:"Cuarto del Rescate", zona:"Cajamarca", img:"/images/cajamarca.jpg" },
-  { nombre:"Banos del Inca", zona:"Cajamarca", img:"/images/cajamarca.jpg" }
+  { nombre:"Plaza de Armas de Cajamarca", zona:"Cajamarca", img:"https://upload.wikimedia.org/wikipedia/commons/8/8e/Plaza_de_Armas_de_Cajamarca.jpg" },
+  { nombre:"Catedral de Cajamarca", zona:"Cajamarca", img:"https://upload.wikimedia.org/wikipedia/commons/8/8e/Plaza_de_Armas_de_Cajamarca.jpg" },
+  { nombre:"Ventanillas de Otuzco", zona:"Cajamarca", img:"https://upload.wikimedia.org/wikipedia/commons/6/6e/Ventanillas_de_Otuzco.jpg" },
+  { nombre:"Cuarto del Rescate", zona:"Cajamarca", img:"https://upload.wikimedia.org/wikipedia/commons/2/20/Cuarto_del_Rescate.jpg" },
+  { nombre:"Banos del Inca", zona:"Cajamarca", img:"https://images.unsplash.com/photo-1544551763-46a013bb70d5?auto=format&fit=crop&w=1200&q=80" }
 ];
 
 export const platos = [];
